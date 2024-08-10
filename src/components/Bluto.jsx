32230c65efbf3a848cff3a66ae6e49cfdeb6cd2e
BDS <113209967+BDS-001@users.noreply.github.com>
2024-08-10 10:32:52 -0400

    added more routes to web page

---
+++ src/components/Bluto.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Bluto = () => {
+  return (
+    <>
+      <p>Hi, I am Bluto! i will beat Popeye!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+};
+
+export default Bluto;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import DefaultProfile from "./DefaultProfile";
 import Spinach from "./Spinach";
 import Popeye from "./Popeye";
+import Bluto from "./Bluto";
 
 const Profile = () => {
   const { name } = useParams();
@@ -9,6 +11,9 @@ const Profile = () => {
   return (
     <div>
       <h1>Hello from profile page!</h1>
+      <Link to="/profile/popeye">popeye </Link>
+      <Link to="/profile/spinach">spinach </Link>
+      <Link to="/profile/bluto">bluto </Link>
       <p>So, how are you?</p>
       <hr />
       <h2>The profile visited is here:</h2>
@@ -16,6 +21,8 @@ const Profile = () => {
         <Popeye />
       ) : name === "spinach" ? (
         <Spinach />
+      ) : name === "bluto" ? (
+        <Bluto />
       ) : (
         <DefaultProfile />
       )}
